f0989085e62606b627c0b619c1efbb6b52d4b39b
Charlie Steenhagen <csteen1005@gmail.com>
2019-10-06 12:27:23 -0400

    Got tests passing

---
+++ tests/Unit/Backend/BoxUserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BoxUserTest extends TestCase
+{
+    public function
+}
