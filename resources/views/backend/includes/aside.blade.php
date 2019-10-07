dc9b19f29bc6ce8997229d7158e965778ffe59ea
Charlie Steenhagen <csteen1005@gmail.com>
2019-10-06 21:18:28 -0400

    Updated aside box select width show logic

--- resources/views/backend/includes/aside.blade.php
+++ resources/views/backend/includes/aside.blade.php
@@ -1,6 +1,6 @@
 <aside class="aside-menu">
     <ul class="nav w-100">
-        <li class="nav-item d-md-none d-lg-none d-xl-none">
+        <li class="nav-item d-lg-none d-xl-none">
             <form id="box_selection_form_aside" action="{{route('admin.updateActiveBox')}}" method="POST">
                 @csrf
                 <select class="box-select-aside show-tick" title="Active Box" data-live-search="true" data-width="fit" name="active-box">
