643bdd1b59942dc654657120e8cbc31a14815acf
Charlie Steenhagen <csteen1005@gmail.com>
2019-10-08 18:05:53 -0400

    Changed to square switch

--- resources/views/backend/auth/features/includes/feature_row.blade.php
+++ resources/views/backend/auth/features/includes/feature_row.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td>{{ ucwords($item->name) }}</td>
     <td class="text-center">
-        <label class="switch switch-pill switch-success m-0 pb-0 pt-1">
+        <label class="switch switch-success m-0 pb-0 pt-1">
             <input type="checkbox" class="switch-input" data-id="{{$item->id}}" data-url="{{route('admin.auth.features.toggle', $item->id)}}" id="customSwitch{{$item->id}}" {{$item->is_active ? "checked" : ""}}>
             <span class="switch-slider"></span>
         </label>
